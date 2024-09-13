48473a3d0156a49c6c01281919329c2b55832379
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-09-13 13:54:05 +0100

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Category from "./Pages/Category";
 import Footer from "./Components/Footer";
-import  Home from "./Pages/Home";
+import Home from "./Pages/Home";
 import { Details } from "./Pages/Details";
-import { Content } from "./Pages/Content";
+// import { Content } from "./Pages/Content"; // there's no "Content" file in your pages folder
 import { Payment } from "./Pages/Payment";
-
 
 export default function App() {
   return (
@@ -14,16 +13,14 @@ export default function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/category" element={<Category/>}/>
-          <Route path="/details" element={<Details/>}/>
-          <Route path="/content" element={<Content/>}/>
-          <Route path="/payment" element={<Payment/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/details" element={<Details />} />
+          {/* <Route path="/content" element={<Content />} /> */}
+          <Route path="/payment" element={<Payment />} />
         </Routes>
-      <Footer/>
+        <Footer />
       </BrowserRouter>
-      </div>
-    
-  )
+    </div>
+  );
 }
-
